c16fc05a17b63e6a15288163b450bda2d9fe887d
TienAnh1312 <buitienanh13122003@gmail.com>
2023-11-02 19:49:00 +0700

    Lab05

---
+++ BuiTien Anh -TTCD - FE/ASP/lab05/lab05/Controllers/AccountController.cs
@@ -0,0 +1,97 @@
+﻿using lab05.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.RegularExpressions;
+
+namespace lab05.Controllers
+{
+    public class AccountController : Controller
+    {
+        [AcceptVerbs("GET", "POST")]
+        public IActionResult VerifyPhone(string phone)
+        {
+            Regex _isPhone = new Regex(@"^\(?([0-9]{3})\)?[-.]?[0-9]{3}[-. ]?([0-9]{4})$");
+            if (!_isPhone.IsMatch(phone))
+            {
+                return Json($"Số điện thoại {phone} không đúng định dạng, VD: 0828345288");
+            }
+            return Json(true);
+        }
+        // GET: AccountController
+        public ActionResult Index()
+        {
+            List<Account> accounts = new List<Account>();
+            return View();
+        }
+
+        // GET: AccountController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: AccountController/Create
+        public ActionResult Create()
+        {
+            Account model = new Account();
+            return View(model);
+        }
+
+        // POST: AccountController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AccountController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: AccountController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AccountController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: AccountController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ BuiTien Anh -TTCD - FE/ASP/lab05/lab05/Models/Account.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace lab05.Models
+{
+    public class Account
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [
+            Display(Name = "Họ và tên"),
+            Required(ErrorMessage = "Họ và tên không được để trống"),
+            MinLength(6, ErrorMessage = "Họ tên ít nhất là 6 ký tự"),
+            MaxLength(20, ErrorMessage = "Họ tên tối đa 20 ký tự")
+        ]
+        public string FullName { get; set; }
+        [Display (Name = "Địa chỉ Email")]
+        [Required(ErrorMessage = "Địa chỉ Email không được trống")]
+        [EmailAddress(ErrorMessage = "Địa chỉ email không đúng định dạng")]
+        public string Email { get; set; }
+
+        [Display(Name = "Số điện thoại")]
+        [DataType(DataType.PhoneNumber)]
+        //[RegularExpression(@"^\(?([0-9]{3})\)?[-.]?[0-9]{3}[-. ]?([0-9]{4})$", ErrorMessage ="Số điện thoại không đúng định dạng")]
+        [Remote(action:"Verifyphone", controller:"Account")]
+        [Required(ErrorMessage = "Số điện thoại không được trống")]
+        public string Phone { get; set; }
+
+        [Display(Name = "Địa chỉ thường trú")]
+        [Required(ErrorMessage = "Địa chỉ không được để trống")]
+        [StringLength(35,ErrorMessage = "Địa chỉ không vượt quá 35 ký tự")]
+        public string Address { get; set; }
+
+        [Display(Name = "Ảnh đại diện")]
+        public string Avatar { get; set; }
+
+        [Display(Name = "Ngày sinh")]
+        [Required(ErrorMessage = "Ngày sinh không được trống")]
+        [DataType(DataType.Date)]
+        public DateTime Birthday { get; set; }
+
+        [Display(Name = "Giới tính")]
+        public string Gender { get; set; }
+
+        [Display(Name = "Mật khẩu")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Link Fb cá nhân")]
+        [Required(ErrorMessage = "Link không được để trống")]
+        [Url(ErrorMessage ="Url phải đúng định dạng bao gồm http hoặc https, tên miền VD: htttp://facebook.com/itvnsoft")]
+        public string Facebook { get; set; }
+
+    }
+}
